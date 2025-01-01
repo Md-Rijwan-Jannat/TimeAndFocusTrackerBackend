355449d702de43b5c0cf0f32ff4aab5348e07093
Md Rijwan Jannat <rijwanjannat36@gmail.com>
2025-01-01 19:52:18 +0600

    migarte postgresql

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/v1/auth", authRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "The Focus Tracker server is running",
+  });
+});
+
+export default app;
